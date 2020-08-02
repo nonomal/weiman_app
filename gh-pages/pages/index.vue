c936335c63959c06fb419cd2a7d640f04fa86980
nrop19 <com.nrop19@gmail.com>
2020-08-03 05:57:35 +0800

    20200803

--- gh-pages/pages/index.vue
+++ gh-pages/pages/index.vue
@@ -1,26 +1,16 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        gh-page
-      </h1>
+      <Logo/>
+      <h1 class="title">微漫App</h1>
+      <h2 class="title">最新版本 {{version}}</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
+        <a href="https://github.com/nrop19/weiman_app/releases/latest" target="_blank" rel="noopener noreferrer"
+           class="button--green">
+          最新版下载页面
         </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
+        <a href="https://github.com/nrop19/weiman_app" target="_blank" rel="noopener noreferrer" class="button--grey">
+          GitHub开源
         </a>
       </div>
     </div>
@@ -28,24 +18,30 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+  import Vue from 'vue'
+  import fs from 'fs'
+  // @ts-ignore
+  import { version } from '../../config.json'
 
-export default Vue.extend({})
+  export default Vue.extend({
+    data () {
+      return { version }
+    },
+  })
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
 
-.title {
-  font-family:
-    'Quicksand',
+  .title {
+    font-family: 'Quicksand',
     'Source Sans Pro',
     -apple-system,
     BlinkMacSystemFont,
@@ -54,22 +50,31 @@ export default Vue.extend({})
     'Helvetica Neue',
     Arial,
     sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+    display: block;
+    font-weight: 100;
+    /*font-size: 100px;*/
+    color: #35495e;
+    letter-spacing: 1px;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  h1 {
+    font-size: 70px;
+    margin: 0;
+  }
 
-.links {
-  padding-top: 15px;
-}
+  h2 {
+    font-size: 30px;
+  }
+
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
+
+  .links {
+    padding-top: 15px;
+  }
 </style>
