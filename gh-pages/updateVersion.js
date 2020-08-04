5f1a85c7763f2b070a0df75f3f4df3dcad91aa68
nrop19 <com.nrop19@gmail.com>
2020-08-05 00:55:21 +0800

    20200805

--- gh-pages/updateVersion.js
+++
@@ -1,9 +0,0 @@
-const YAML = require('yaml')
-const fs = require('fs')
-
-const yaml = YAML.parse(fs.readFileSync('../pubspec.yaml', 'utf8'))
-
-const version = yaml.version.split('+')[0]
-
-const template = fs.readFileSync('./pages/index.vue', 'utf8')
-fs.writeFileSync('./pages/index.vue', template.replace('[version]', version))
